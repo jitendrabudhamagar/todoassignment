7abc184edaac215a6c958acb2552688cd71d8cad
jitendrabudhamagar <t3buji00@students.oamk.fi>
2024-11-07 22:13:07 +0200

    todo part 3 complited

--- server/index.js
+++ server/index.js
@@ -9,24 +9,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.post('/create', (req, res) => {
-    const pool = openDb()
-
-    pool.query('insert into task (description) values ($1 returning *', 
-        [req.body.title, req.body.description], 
+    const pool = openDb();
+    
+    pool.query(
+        'INSERT INTO task (description) VALUES ($1) RETURNING *', 
+        [req.body.description], 
         (error, result) => {
-        if (error) {
-            return res.status(500).json({error: error.message})
+            if (error) {
+                return res.status(500).json({ error: error.message });
+            }
+            res.status(200).json({ id: result.rows[0].id });
         }
-        res.status(200).json({id: result.rows[0].id})
-    })
-    })
+    );
+});
+
 
 
 app.get('/', (req, res) => {
     const pool = openDb()
     pool.query('SELECT * FROM task', (error, result) => {
         if (error) {
-            return res.status(500).json({error: error.message})
+            return res.status(500).json({ error: error.message })
         }
         res.status(200).json(result.rows)
     })
@@ -36,11 +39,11 @@ app.delete('/delete/:id', (req, res) => {
     const pool = openDb()
     const id = parseInt(req.params.id)
     pool.query('delete from task where id = $1', [id], (error, result) => {
-    
+
         if (error) {
-            return res.status(500).json({error: error.message})
+            return res.status(500).json({ error: error.message })
         }
-        res.status(200).json({id: id})
+        res.status(200).json({ id: id })
     })
 });
 
@@ -57,6 +60,6 @@ const openDb = () => {
 }
 
 
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`)
-    })
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
